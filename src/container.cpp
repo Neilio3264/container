5e16995effcc52f7706330f49b324f2fdf095a19
Neilio3264 <neilrathod12@gmail.com>
2024-03-28 11:39:29 -0500

    Clone sys call to create child process

--- src/container.cpp
+++ src/container.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <sys/wait.h>
 
-int main()
+char *stack_memory()
 {
-    std::cout << "Hello, World! " << std::endl;
+    const int stackSize = 65536;
+    auto *stack = new (std::nothrow) char[stackSize];
+
+    if (!stack)
+    {
+        std::cerr << "Cannot allocate memory " << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    return stack + stackSize;
+}
+
+int jail(void *args)
+{
+    std::cout << "Hello !! ( child ) " << std::endl;
+    return EXIT_SUCCESS;
+}
+
+int main(int argc, char **argv)
+{
+    std::cout << "Hello, World! ( parent ) " << std::endl;
+
+    clone(jail, stack_memory(), SIGCHLD, 0);
+    wait(nullptr);
     return EXIT_SUCCESS;
 }
