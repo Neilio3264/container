6dc142de61f9d55192ba5138e6310a6a434fff86
Neilio3264 <neilrathod12@gmail.com>
2024-03-29 13:48:39 -0500

    mount resources to container

--- src/container.cpp
+++ src/container.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <sys/mount.h>
 
 char *stack_memory()
 {
@@ -19,16 +20,36 @@ char *stack_memory()
 template <typename... P>
 int run(P... params)
 {
-    clearenv();
     char *args[] = {(char *)params..., (char *)0};
     return execvp(args[0], args);
+}
+
+void setup_variables()
+{
+    clearenv();
+    setenv("TERM", "xterm-256color", 0);
+    setenv("PATH", "bin/:/sbin/:usr/bin:/usr/sbin", 0);
+}
+
+void setup_root(const char *folder)
+{
+    chroot(folder);
+    chdir("/");
 }
 
 int jail(void *args)
 {
     std::cout << "Hello, World! ( child ) " << std::endl;
     std::cout << "Child ID: " << getpid() << std::endl;
+
+    setup_variables();
+    setup_root("./utils/root/");
+
+    mount("proc", "/proc", "proc", 0, 0);
+
     run("/bin/sh");
+
+    umount("/proc");
     return EXIT_SUCCESS;
 }
 
